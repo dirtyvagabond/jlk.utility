bda2af8b1088fa4f2894b98d3afd688b693704d4
JLK <taprisiot@rocketmail.com>
2012-05-13 21:35:37 +1000

    update url

--- project.clj
+++ project.clj
@@ -1,7 +1,6 @@
 (defproject jlk/utility "0.1-SNAPSHOT"
   :description "programming utility functions"
-  :url ""
-  :license {:name "BSD"
-            :url "LICENSE"}
+  :url "https://github.com/taprisiot/jlk.utility"
+  :license {:name "BSD"}
   :dependencies [[org.clojure/clojure "1.4.0"]]
   :plugins [[lein-swank "1.4.0"]])
