83b3ebe4183c5971a402950fe471eb397498b86f
JLK <taprisiot@rocketmail.com>
2012-05-15 18:09:42 +1000

    make get-noisy more flexible

--- src/jlk/utility/core.clj
+++ src/jlk/utility/core.clj
@@ -15,7 +15,9 @@
 
 (defn get-noisy
   "like get, but raise exception if key not found"
-  [map key]
-  (if-let [rs (get map key)]
-    rs
-    (exception "key %s not found in map" key)))
+  ([map key]
+     (get-noisy map key "key %s not found in map" key))
+  ([map key message & args]
+     (if-let [rs (get map key)]
+       rs
+       (apply exception message args))))
