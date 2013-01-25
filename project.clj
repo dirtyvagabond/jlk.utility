4acf2e4339db3ac9177172c1da94a09ab65ba887
Aaron Crow <dirtyvagabond@yahoo.com>
2013-01-25 14:01:12 -0800

    So to release a non-snapshot version

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject jlk/utility "0.1-SNAPSHOT"
+(defproject factual/jlk-utility "0.1"
   :description "programming utility functions"
-  :url "https://github.com/taprisiot/jlk.utility"
+  :url "https://github.com/dirtyvagabond/jlk.utility"
   :license {:name "BSD"}
   :dependencies [[org.clojure/clojure "1.4.0"]]
   :plugins [[lein-swank "1.4.0"]])
